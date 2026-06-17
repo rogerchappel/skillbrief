f75ddd8e52556bca80587f7f5037bbac4ffd13dc
Roger Chappel <miscanalysis@gmail.com>
2026-06-17 16:54:54 +1000

    Tighten release readiness metadata

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,26 @@
 #!/usr/bin/env node
 const fs = require('node:fs');
 const { buildBrief, parseInput } = require('./index');
+
+const version = require('../package.json').version;
+
 function readSource(argv) {
   const file = argv[2];
   if (!file || file === '-') return fs.readFileSync(0, 'utf8');
   return fs.readFileSync(file, 'utf8');
 }
+
+const arg = process.argv[2];
+if (arg === '--help' || arg === '-h') {
+  process.stdout.write('Usage: skillbrief <brief-input.json|->\n\nBuild a traceable repo-to-content brief from local JSON input.\n');
+  process.exit(0);
+}
+
+if (arg === '--version' || arg === '-v') {
+  process.stdout.write(`${version}\n`);
+  process.exit(0);
+}
+
 try {
   const input = parseInput(readSource(process.argv));
   process.stdout.write(buildBrief(input));
